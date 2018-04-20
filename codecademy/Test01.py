aecfd7f66489e6ecffa39195d34307604d8da09c
gha <nanocean@gmail.com>
2018-04-20 17:46:15 +0900

    파이썬 연습

--- codecademy/Test01.py
+++ codecademy/Test01.py
@@ -1,70 +1,73 @@
-from random import randint
+def is_int(x):
+    while int(x) != x:
+        x *= 10
 
-board = []
-limit = 4
+        if x % 10 != 0:
+            return False
+    else:
+        return True
 
-for x in range(5):
-    board.append(["O"] * 5)
+# print(is_int(1.0000))
+
+# def digit_sum(n):
+#     sum = 0
+#     while n > 0:
+#         sum += n % 10
+#         n = int(n / 10)
+#
+#     return sum
+
+# def digit_sum(n):
+#     if n <= 0:
+#         return n
+#     else:
+#         return (n % 10) + digit_sum(n // 10)
+#
+# print(digit_sum(12345))
+
+# print(12//10)
+
+# def factorial(x):
+#     r = 1
+#     for i in range(1, x + 1):
+#         r *= i
+#
+#     return r
+
+# def factorial(n):
+#     if n < 2:
+#         return n
+#     else:
+#         return n * factorial(n - 1)
+#
+# print(factorial(4))
+#
+# def is_prime(x):
+#     if x == 1 or x == 2:
+#         return True
+#
+#     for i in range(2, x):
+#         if x % i == 0:
+#             return False
+#     else:
+#         return True
+#
+# print(is_prime(10))
+
+# def reverse(s):
+#     r = ""
+#     for index, value in enumerate(s):
+#         r += s[len(s) - index - 1]
+#     return r
+#
+# print(reverse("asdf"))
 
 
-def print_board(board):
-    for row in board:
-        print
-        " ".join(row)
+# print(type(reversed("asdf")))
 
+l = reversed("asdf")
+
+for e in l:
+    print(e),
 
 print
-"Let's play Battleship!"
-print_board(board)
-
-
-def random_row(board):
-    return randint(0, len(board) - 1)
-
-
-def random_col(board):
-    return randint(0, len(board[0]) - 1)
-
-
-ship_row = random_row(board)
-ship_col = random_col(board)
-# print(ship_row)
-# print(ship_col)
-
-
-def number_input(message):
-    try:
-        return int(raw_input(message))
-    except ValueError:
-        return number_input(message)
-
-
-# 이 아래 부분은 전부 다 for 반복문 안에 들어가야 합니다!
-# 각 줄마다 네 칸씩 들여쓰기 해야하는 것, 잊지마세요!
-for turn in range(4):
-    print("turn: " + str(turn + 1))
-
-    guess_row = int(number_input("Guess Row:"))
-    guess_col = int(number_input("Guess Col:"))
-
-    if guess_row == ship_row and guess_col == ship_col:
-        print
-        "Congratulations! You sunk my battleship!"
-        break
-    else:
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print
-            "Oops, that's not even in the ocean."
-        elif (board[guess_row][guess_col] == "X"):
-            print
-            "You guessed that one already."
-        else:
-            print
-            "You missed my battleship!"
-            board[guess_row][guess_col] = "X"
-
-        if (turn == 3):
-            print
-            "Game Over"
-
-print_board(board)
